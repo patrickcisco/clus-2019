32bcbf85b842ff18cd1d18353ddff6a9224a5b88
priel <priel@cisco.com>
2019-06-10 12:14:20 -0700

    add httprouter example

---
+++ server/demo/main.go
@@ -0,0 +1,23 @@
+// https://github.com/julienschmidt/httprouter
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+}
