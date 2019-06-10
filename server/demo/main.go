a722c98b53157968a431a2ca8f2c6414a6972fe9
priel <priel@cisco.com>
2019-06-10 12:57:10 -0700

    add echo sample to webserver

--- server/demo/main.go
+++ server/demo/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -16,8 +17,18 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
+func Echo(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	for k, v := range r.Header {
+		fmt.Println(k, v)
+	}
+
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
+	router.POST("/echo", Echo)
+	log.Fatal(http.ListenAndServe("localhost:8070", router))
 }
